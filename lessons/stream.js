844e734a787151181c27ffff09844a3b68f9f0c1
YuriyWeb <yuriy.mayorov.web@mail.ru>
2022-11-11 21:16:49 +0400

    add test.txt

---
+++ lessons/stream.js
@@ -0,0 +1,8 @@
+//Readable - чтение
+//Writable - запись
+// Duplex - для чтения и записи Readable + Writable
+// Transform - такой же как и Duplex, но может изменить данные
+
+const fs = require('fs')
+const path = require('path')
+
