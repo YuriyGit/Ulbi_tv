743c1967ca9ca0b7d858d2388b4ae3b5cd62eca3
YuriyWeb <yuriy.mayorov.web@mail.ru>
2022-11-11 22:54:40 +0400

    mod index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const http = require('http')
+
 const PORT = process.env.PORT || 5000
+
 const server = http.createServer((req, res) => {
-    res.end('Server работает!')
+    res.writeHead(200, {
+        'contentType': 'application/json'
+    })
+    if (req.url === '/users') {
+        return res.end(JSON.stringify([
+            {id: 1, name: "Yuriy"}
+        ]))
+    }
+    if (req.url === ('/posts')) {
+        return res.end('POSTS')
+    }
+
+    res.end(req.url)
 })
 
-server.listen(PORT, ()=>console.log(`Server start on PORT ${PORT}`))
+server.listen(PORT, () => console.log(`Server start on PORT ${PORT}`))
